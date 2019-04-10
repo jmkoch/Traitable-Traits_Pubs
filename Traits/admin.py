151792effd833fd13431c3c78f7c1611307eae46
jmkoch <jordan.marie.koch@gmail.com>
2019-04-09 19:53:27 -0500

    updated admin.py:

--- Traits/admin.py
+++ Traits/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from Pubs.models import Pub
+from Traits.models import Trait
+from import_export.admin import ImportExportModelAdmin
+from Traits.resources import TraitResource
 
-# Register your models here.
+# defining TraitAdmin class (useful & necessary for django-import-export module)
+class TraitAdmin(ImportExportModelAdmin):
+	list_display = ('id', 'genus', 'species', 'isi', 'fruit_type', 'pub_reference')
+	form = TraitForm
+	resource_class = TraitResource
+
+class TraitInline(admin.TabularInline):
+	model = Trait
+	show_change_link = True
+
+# registering our models - very important step!! if you forget to register a model, it won't show up on admin page.
+admin.site.register(Trait, TraitAdmin)
