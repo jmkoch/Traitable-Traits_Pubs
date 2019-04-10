7e4d0d24d088dcb0a55d62c20ab815da0b48fad3
jmkoch <jordan.marie.koch@gmail.com>
2019-04-09 19:53:03 -0500

    updated admin.py

--- Pubs/admin.py
+++ Pubs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from Pubs.models import Pub
+from Traits.models import Trait
+from import_export.admin import ImportExportModelAdmin
+from Pubs.resources import PubResource
 
-# Register your models here.
+# defining PubAdmin class (useful & necessary for django-import-export module)
+class PubAdmin(ImportExportModelAdmin):
+	list_display = ('id', 'title', 'lastName', 'middleName', 'firstName', 'citekey', 'pub_type')
+	form = PubForm
+	resource_class = PubResource
+	show_change_link = True
+	inlines = [
+		TraitInline,
+	] # this inline adds Traits to Pub admin page to allow user to upload pub and trait in parallel
+
+class PubInLine(admin.TabularInline):
+	model = Pub
+
+# registering our models - very important step!! if you forget to register a model, it won't show up on admin page.
+admin.site.register(Pub, PubAdmin)
