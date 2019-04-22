35ab771a49b1769ff13c604207842ff5a2bb7bad
jmkoch <jordan.marie.koch@gmail.com>
2019-04-22 15:11:07 -0500

    removed incorrect PersonInline

--- Traits/admin.py
+++ Traits/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from Pubs.models import Pub
-from Traits.models import Trait
+from Traits.models import Trait, Person
 from import_export.admin import ImportExportModelAdmin
 from Traits.resources import TraitResource
 
@@ -8,6 +8,7 @@ from Traits.resources import TraitResource
 class TraitAdmin(ImportExportModelAdmin):
 	list_display = ('id', 'genus', 'species', 'isi', 'fruit_type', 'pub_reference')
 	resource_class = TraitResource
+	#list_filter = ()
 
 class TraitInline(admin.TabularInline):
 	model = Trait
@@ -15,3 +16,4 @@ class TraitInline(admin.TabularInline):
 
 # registering our models - very important step!! if you forget to register a model, it won't show up on admin page.
 admin.site.register(Trait, TraitAdmin)
+admin.site.register(Person)
--- Traits/models.py
+++ Traits/models.py
@@ -27,3 +27,45 @@ class Trait(models.Model):
 
     def __str__(self):
         return (str(self.genus)+' '+str(self.species))
+
+class RestrictedManager(models.Manager):
+    ''' This manager filters out deleted sets'''
+    def get_queryset(self):
+        results = super(RestrictedManager, self).get_queryset().filter(safe_deleted=False)
+        return results.filter(safe_deleted=False)
+    def all_with_deleted(self):
+        return super(RestrictedManager, self).get_queryset()
+    def deleted_set(self):
+        return super(RestrictedManager, self).get_queryset().filter(safe_deleted=True)
+
+class Person(models.Model):
+    first_names = models.CharField(max_length=100, null=True, blank=True)
+    middle_names = models.CharField(max_length=100, null=True, blank=True)
+    last_names = models.CharField(max_length=100, null=True, blank=True)
+
+    #flag kept for safe deletion
+    safe_deleted=models.BooleanField(null=False,default=False)
+
+    objects = models.Manager()
+    rmobjects = RestrictedManager()
+
+    def __unicode__(self):
+        name = ''
+        if self.first_names!=None:
+            name += str(self.first_names)
+        if self.middle_names!=None:
+            name += " "+str(self.middle_names)
+        if self.last_names !=None:
+            name += " "+ str(self.last_names)
+        return name
+
+
+    def __str__(self):
+        name = ''
+        if self.first_names!=None:
+            name += str(self.first_names)
+        if self.middle_names!=None:
+            name += " "+str(self.middle_names)
+        if self.last_names !=None:
+            name += " "+ str(self.last_names)
+        return name
