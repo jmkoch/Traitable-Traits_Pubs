fa02ff18bbb598e30487f2214ad2d12fa81de12f
jmkoch <jordan.marie.koch@gmail.com>
2019-04-23 10:55:03 -0500

    tried adding a TextWidget but it didn't work

--- Traits/forms.py
+++ Traits/forms.py
@@ -1,11 +1,14 @@
 from dal import autocomplete
 from django import forms
 from Traits.models import Trait
+#import autocomplete_light
 
 class TraitForm(forms.ModelForm):
 	class Meta:
 		model = Trait
 		fields = ('__all__')
 		widgets = {
-			'genus': autocomplete.ListSelect2(url='trait-autocomplete')
+			'genus': autocomplete.TagSelect2(url='trait-autocomplete')
+			#'genus': autocomplete.TextWidget('GenusAutocomplete')
 		}
+
