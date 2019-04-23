7ae17e9a817ac19c235e56281b27a921539d629a
jmkoch <jordan.marie.koch@gmail.com>
2019-04-23 09:13:03 -0500

    added Trait autocomplete view

--- Traits/views.py
+++ Traits/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from dal import autocomplete
+from Traits.models import Trait
 
-# Create your views here.
+class TraitAutocomplete(autocomplete.Select2QuerySetView):
+	def get_queryset(self):
+		if not self.request.is_authenticated():
+			return Trait.objects.none()
+
+		qs = Trait.objects.all()
+
+		if self.q:
+			qs = qs.filter(name__istartswith=self.q)
+
+		return self.q
