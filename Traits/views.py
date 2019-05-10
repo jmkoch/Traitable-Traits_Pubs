387fd4c0a60f8f17785adb4082d54084d2d5fe99
jmkoch <jordan.marie.koch@gmail.com>
2019-05-10 08:58:59 -0500

    added view for Traitable homepage

--- Traits/views.py
+++ Traits/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from dal import autocomplete
 from Traits.models import Trait
 
+def welcome_page(request):
+	return render(request, 'Traits/welcome_page.html', {})
+
 class TraitAutocomplete(autocomplete.Select2QuerySetView):
 	def get_queryset(self):
 		if not self.request.is_authenticated():
