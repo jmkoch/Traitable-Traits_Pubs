e21d443036bf5462e557cebc96f8852f748f95b6
jmkoch <jordan.marie.koch@gmail.com>
2019-05-20 10:12:25 -0500

    fixed model type so that import/export features work again in Traits

--- Traits/admin.py
+++ Traits/admin.py
@@ -2,43 +2,19 @@ from django.contrib import admin
 from Pubs.models import Pub
 from Traits.models import Trait
 #from import_export import resources
-from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin, ImportExportMixin, ImportMixin, ExportActionModelAdmin
 from Traits.resources import TraitResource
 from Traits.forms import TraitForm
-#from django.contrib.admin import SimpleListFilter
 
-'''
-class TraitListFilter(admin.SimpleListFilter):
-	title = 'genus'
-	parameter_name = 'genus'
-	related_filter_parameter = 'trait__id__exact'
-
-	def lookups(self, request, model_admin):
-		queryset = Trait.objects.order_by('id')
-
-		if self.related_filter_parameter in request.GET:
-			queryset = queryset.filter(id=request.GET[self.related_filter_parameter])
-
-		#for genus in queryset:
-		#	list_of_questions.append(
-		#		(str(genus.id), genus.genus)
-		#	)
-		#return sorted(list_of_questions, key=lambda tp: tp[1])
-
-	def queryset(self, request, queryset):
-		if self.value():
-			return queryset.filter(id = self.value())
-		return queryset
-'''
-
+# defining TraitInLine class (to have trait lines show up in Pub admin)
 class TraitInline(admin.TabularInline):
 	model = Trait
 	show_change_link = True
 	extra = 0  # this removes the blank data rows below; replaces them with 'add trait' option link.
 
 # defining TraitAdmin class (useful & necessary for django-import-export module)
-#class TraitAdmin(ImportExportModelAdmin):
-class TraitAdmin(admin.ModelAdmin):
+class TraitAdmin(ImportExportModelAdmin):
+#class TraitAdmin(admin.ModelAdmin):
 	autocomplete_fields = ['pub_reference'] #makes citekey an autocomplete field within Traits admin
 	list_display = ('id', 'genus', 'species', 'isi', 'fruit_type', 'pub_reference')
 	list_display_links = ('pub_reference', 'id')  # makes the citekey linkable on Trait admin interface
